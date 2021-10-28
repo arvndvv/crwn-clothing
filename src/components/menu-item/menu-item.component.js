b5567466653741793d7ee181c4b53a82fb49cca4
Aravind V V <arvndvv@gmail.com>
2021-10-28 16:27:15 +0530

    fix: ui

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,14 +1,11 @@
 import React from "react";
 import './menu-item.styles.scss'
 const MenuItem = ({ title, imageUrl, size }) => (
-    <div style={{backgroundImage:`url(${imageUrl})`}}>
-
-    <div className={`${size} menu-item`} >
+    <div className={`${size ? size : ''} menu-item`} style={{backgroundImage:`url(${imageUrl})`}}>
     <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
     </div>
 </div>
-    </div>
 )
 export default MenuItem;
