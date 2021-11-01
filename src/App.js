a513fca7714236bd09b03e26b7c8653ca15d7353
Aravind V V <arvndvv@gmail.com>
2021-11-01 23:56:47 +0530

    route-base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import './App.css';
 import Home from './pages/home/home.component';
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 function App() {
   return (
     <div className="App">
- <Home/>
+      <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/hats" component={HatsPage} />
+
+      </Switch>
     </div>
   );
 }
