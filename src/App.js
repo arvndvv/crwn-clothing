c6c88bfd6644655ebe9f8396663f6640ee2075e4
Aravind V V <arvndvv@gmail.com>
2022-01-15 22:39:15 +0530

    firebase, login

--- src/App.js
+++ src/App.js
@@ -6,14 +6,44 @@ import Home from './pages/home/home.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.component';
-import {auth} from './firebase/firebase.utils';
-class App extends React.component {
-  constructor(props) {
-    super(props);
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+class App extends React.Component {
+  constructor() {
+    super();
     this.state = {
       currentUser: null
     }
   }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // this.setState({ currentUser: user });
+      console.log(userAuth);
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          },()=>{})
+          console.log(snapShot.data());
+        })
+      }
+      else{
+        this.setState({currentUser: userAuth})
+      }
+      // createUserProfileDocument(user)
+    });
+  }
+  
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
   render() {
     return (
       <div className="App">
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,25 +1,49 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore'; 
-import 'firebase/compat/auth';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAkT-3w-IN8jh3Y43swl64hYqmDZTOseG8",
-    authDomain: "crwn-site.firebaseapp.com",
-    projectId: "crwn-site",
-    storageBucket: "crwn-site.appspot.com",
-    messagingSenderId: "840108482451",
-    appId: "1:840108482451:web:292b7f47c32e6e19fd9266",
-    measurementId: "G-X48HB6MN7N"
-  };
+  apiKey: "AIzaSyAkT-3w-IN8jh3Y43swl64hYqmDZTOseG8",
+  authDomain: "crwn-site.firebaseapp.com",
+  projectId: "crwn-site",
+  storageBucket: "crwn-site.appspot.com",
+  messagingSenderId: "840108482451",
+  appId: "1:840108482451:web:292b7f47c32e6e19fd9266",
+  measurementId: "G-X48HB6MN7N",
+};
 
-  // Initialize Firebase
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.collection("users").doc(userAuth.uid);
+  const snapShot = await userRef.get();
+  if(!snapShot.exists){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+    try{
+        await userRef.set({
+            displayName,
+            email,
+            createdAt,
+            ...additionalData
+        })
+    }
+    catch(err){
+        console.log('error creating the user',err.message);
+    }
+    console.log(snapShot);
+  }
+  return userRef;
+};
+
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
