9ca968203439d391f937da73ee5cd24bd6eb8d84
Aravind V V <arvndvv@gmail.com>
2022-01-16 21:11:39 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import { Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home/home.component';
 import ShopPage from './pages/shop/shop.component';
