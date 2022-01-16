9a3e06e97a90a6bd3c451b0d9de192031cd40821
Aravind V V <arvndvv@gmail.com>
2022-01-16 16:38:41 +0530

    fix

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -19,7 +19,7 @@ class SignIn extends Component{
     const {email,password} = this.state;
 
     try{
-        await auth. (email,password);
+        await auth.signInWithEmailAndPassword(email,password);
         this.setState({email:'',password:''})
     }
     catch(error){
